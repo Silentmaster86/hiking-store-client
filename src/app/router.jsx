94b5e59748fe9d18db5856db85f7ae31a21601ba
Przemysław Pietkun <116354099+Silentmaster86@users.noreply.github.com>
2026-01-16 16:12:49 +0000

    Set up routing with createBrowserRouter

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,1 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import ProductsPage from "../pages/ProductsPage";
 
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <ProductsPage /> },
+      { path: "products", element: <ProductsPage /> },
+    ],
+  },
+]);
