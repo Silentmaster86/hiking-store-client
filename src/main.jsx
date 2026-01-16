72a46d9596df7870f8b233c07b5c83e9b7bebf0d
Przemysław Pietkun <przemyslaw.pietkun@gmail.com>
2026-01-16 23:50:24 +0000

    Fix theme export/import

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
 import App from "./app/App.jsx";
 import GlobalStyles from "./styles/GlobalStyles.js";
-import theme from "./styles/theme.js";
+import { theme } from "./styles/theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
