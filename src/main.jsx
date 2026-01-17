723241fe2d715764c09bdb9458d621cf81c22db3
Przemysław Pietkun <116354099+Silentmaster86@users.noreply.github.com>
2026-01-17 07:19:59 +0000

    Fix import statements for GlobalStyles and theme

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
 import App from "./app/App.jsx";
-import { GlobalStyles } from "./styles/GlobalStyles.js";
-import { theme } from "./styles/theme.js";
+import GlobalStyles from "./styles/GlobalStyles.js";
+import theme from "./styles/theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
