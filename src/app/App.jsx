3fa30d150e9f49c7141e5cbbce6a605244cf835d
Przemysław Pietkun <116354099+Silentmaster86@users.noreply.github.com>
2026-01-17 07:31:43 +0000

    Wrap routes in Layout component

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
 import HomePage from "../pages/HomePage";
 import ProductsPage from "../pages/ProductsPage";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/products" element={<ProductsPage />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Route>
     </Routes>
   );
 }
