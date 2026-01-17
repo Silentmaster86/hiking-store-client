bd9593f2282404ccd8d34e9270069e3334c21eaf
Przemysław Pietkun <116354099+Silentmaster86@users.noreply.github.com>
2026-01-17 07:25:11 +0000

    Change default route to HomePage component

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,11 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage";
 import ProductsPage from "../pages/ProductsPage";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/products" replace />} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/products" element={<ProductsPage />} />
     </Routes>
   );
