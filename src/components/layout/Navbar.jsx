c75d008ec59921f90d4b674d4bea1ade5a3cc303
Przemysław Pietkun <116354099+Silentmaster86@users.noreply.github.com>
2026-01-17 09:40:11 +0000

    Fix import path for useCart in Navbar component

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
-import { useCart } from "../context/CartContext";
+import { useCart } from "../../context/CartContext";
 
 const Bar = styled.header`
   position: sticky;
