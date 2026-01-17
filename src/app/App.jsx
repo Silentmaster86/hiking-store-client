542df5ebf704d24bf812ee5efdd3097b56a08095
Przemysław Pietkun <116354099+Silentmaster86@users.noreply.github.com>
2026-01-17 08:14:41 +0000

    Update import path for Layout component

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Layout from "./Layout";
+import Layout from "../components/layout/Layout";
 import HomePage from "../pages/HomePage";
 import ProductsPage from "../pages/ProductsPage";
 
